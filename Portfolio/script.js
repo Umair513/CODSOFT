a4604c8c7f73dc6c8f8d6c32b6f909519a09ac4a
Umair513 <soomroumair513@gmail.com>
2023-08-24 00:54:41 +0500

    done with pricing section

--- Portfolio/script.js
+++
@@ -1,5 +0,0 @@
-console.log("Script added yes")
-document.querySelector(".cross").style.display = "none"
-document.querySelector("hamburger").addEventListener("click", () => {
-    document.querySelector(".sidebar").classList.toggle("sidebargo")
-})
